a3e099913603593eb520501417b11a39b007661c
Marco Botelho <marcobotelhoabreu@gmail.com>
2024-04-14 23:42:53 -0300

    Atualização de configuração da aplicação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { AuthService } from './auth/auth.service';
+import { AlertService } from './shared/alert/alert.service';
+import { MenuBarComponent } from './shared/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, CommonModule, MenuBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-angular-standalone';
+
+  constructor(private authService: AuthService, private alertService: AlertService, private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  onLogout() {
+    this.authService.onLogout();
+    this.alertService.sendAlert(['auth'], '<strong>Sessão encerrada</strong><br>Efetue o login para continuar', 'success');
+  }
+
+  isAuthenticated(): boolean {
+    return this.authService.isAuthenticated();
+  }
+
+  getUsuarioEmail(): string {
+    return this.authService.getUsuarioEmail();
+  }
+
+  getRoles(): string[] {
+    return this.authService.getRoles();
+  }
+
+  onAlterarSenha() {
+    const token = this.authService.getToken();
+    this.router.navigate(['/senhas', 'alterar', token]);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,142 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
+        canActivate: [AuthGuard], data: { expectedRoles: ['ROLE_ADMIN', 'ROLE_USER'] }
+    },
+    {
+        path: 'auth',
+        loadComponent: () => import('./auth/auth.component').then(m => m.AuthComponent)
+    },
+    {
+        path: 'clientes',
+        children: [
+            {
+                path: "",
+                loadComponent: () =>
+                    import("./clientes/clientes-list/clientes-list.component").then(m => m.ClientesListComponent),
+                canActivate: [AuthGuard], data: { expectedRoles: ['ROLE_ADMIN', 'ROLE_USER'] }
+            },
+            {
+                path: ":clienteId",
+                children: [
+                    {
+                        path: "",
+                        loadComponent: () =>
+                            import("./clientes/clientes-detail/clientes-detail.component").then(m => m.ClientesDetailComponent),
+                        canActivate: [AuthGuard], data: { expectedRoles: ['ROLE_ADMIN', 'ROLE_USER'] }
+                    },
+                    {
+                        path: "telefones",
+                        children: [
+                            {
+                                path: "",
+                                loadComponent: () =>
+                                    import("./clientes/cliente-telefones-list/cliente-telefones-list.component").then(m => m.ClienteTelefonesListComponent),
+                                canActivate: [AuthGuard], data: { expectedRoles: ['ROLE_ADMIN', 'ROLE_USER'] }
+                            },
+                            {
+                                path: ":telefoneId",
+                                loadComponent: () =>
+                                    import("./clientes/cliente-telefones-detail/cliente-telefones-detail.component").then(m => m.ClienteTelefonesDetailComponent),
+                                canActivate: [AuthGuard], data: { expectedRoles: ['ROLE_ADMIN', 'ROLE_USER'] }
+                            }
+                        ]
+                    }
+                ]
+            },
+        ]
+    },
+    {
+        path: 'usuarios',
+        children: [
+            {
+                path: "",
+                loadComponent: () =>
+                    import("./usuarios/usuarios-list/usuarios-list.component").then(m => m.UsuariosListComponent),
+                canActivate: [AuthGuard], data: { expectedRoles: ['ROLE_ADMIN'] }
+            },
+            {
+                path: ":usuarioId",
+                children: [
+                    {
+                        path: "",
+                        loadComponent: () =>
+                            import("./usuarios/usuarios-detail/usuarios-detail.component").then(m => m.UsuariosDetailComponent),
+                        canActivate: [AuthGuard], data: { expectedRoles: ['ROLE_ADMIN'] }
+                    },
+                    {
+                        path: "perfis",
+                        children: [
+                            {
+                                path: "",
+                                loadComponent: () =>
+                                    import("./usuarios/usuario-perfis-list/usuario-perfis-list.component").then(m => m.UsuarioPerfisListComponent),
+                                canActivate: [AuthGuard], data: { expectedRoles: ['ROLE_ADMIN'] }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        path: 'perfis',
+        children: [
+            {
+                path: "",
+                loadComponent: () =>
+                    import("./perfis/perfis-list/perfis-list.component").then(m => m.PerfisListComponent),
+                canActivate: [AuthGuard], data: { expectedRoles: ['ROLE_ADMIN'] }
+            },
+            {
+                path: ":perfilId",
+                children: [
+                    {
+                        path: "",
+                        loadComponent: () =>
+                            import("./perfis/perfis-detail/perfis-detail.component").then(m => m.PerfisDetailComponent),
+                        canActivate: [AuthGuard], data: { expectedRoles: ['ROLE_ADMIN'] }
+                    },
+                    {
+                        path: "usuarios",
+                        children: [
+                            {
+                                path: "",
+                                loadComponent: () =>
+                                    import("./perfis/perfil-usuarios-list/perfil-usuarios-list.component").then(m => m.PerfilUsuariosListComponent),
+                                canActivate: [AuthGuard], data: { expectedRoles: ['ROLE_ADMIN'] }
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        path: "senhas",
+        children: [
+            {
+                path: "reset",
+                loadComponent: () => import("./senhas/senhas-reset/senhas-reset.component").then(m => m.SenhasResetComponent)
+            },
+            {
+                path: "alterar/:token",
+                loadComponent: () => import("./senhas/senhas-alterar/senhas-alterar.component").then(m => m.SenhasAlterarComponent)
+            }
+        ]
+    },
+    {
+        path: '',
+        redirectTo: 'auth',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'auth',
+        pathMatch: 'full'
+    }
+];
